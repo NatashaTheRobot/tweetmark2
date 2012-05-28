7430a630c744803d53bdac73d6abb47bc3a99fb3
Natasha Murashev <nmurashev@gmail.com>
2012-05-28 15:45:20 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweetmark::Application.config.secret_token = '8897ff7b2997384dc939b56af973e6bc7ab816da9c23de021a54871b108764fc6190be1e565c5e67a420a8067e39f2b8b37f6d4c5925933ce9fdd95ac606d56c'
