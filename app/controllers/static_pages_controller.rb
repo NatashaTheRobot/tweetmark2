1944ff97b83a539f18365eb964db8ae2f19327a6
Natasha Murashev <nmurashev@gmail.com>
2012-05-29 21:00:17 -0700

    added hashtags

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,13 @@ class StaticPagesController < ApplicationController
          #get the user's tweets and display them
          @user = User.find(session[:user_id])
          @tweets = @user.tweets
+         @hashtags = []
+         @tweets.each do |tweet|
+             hashtag = Hashtag.find_by_tweet_id(tweet.id)
+             if hashtag != nil
+                 @hashtags << hashtag.text unless @hashtags.include?(hashtag)
+             end 
+         end
       end
   end
 
