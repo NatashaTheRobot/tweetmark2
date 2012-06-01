716818add827060a43673b2021196dd87ee224db
Natasha Murashev <nmurashev@gmail.com>
2012-05-31 17:43:53 -0700

    updated sidebar on tag pages

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -46,6 +46,29 @@ class HashtagsController < ApplicationController
         
         end
         
-        render "static_pages/home"
+        @hashtags = Hash.new # {hashtag => count}
+        if @tweets.length > 0
+            @tweet_ids = []
+            @tweets.each do |tweet|
+                @tweet_ids.push(tweet.id)
+            end
+            @tweetids_with_hashtags = []
+            hashtags = Hashtag.where(:tweet_id => @tweet_ids)
+            if hashtags != []
+                hashtags.each do |hashtag|
+                    @tweetids_with_hashtags << hashtag[:tweet_id] unless @tweetids_with_hashtags.include?(hashtag[:tweed_id])
+                    if @hashtags.has_key?(hashtag[:text])
+                        @hashtags[hashtag[:text]] += 1 
+                    else
+                       @hashtags[hashtag[:text]] = 1
+                    end
+                end
+            end
+            @hashtags = Hash[@hashtags.sort] #sort hashtags alphabetically 
+            @untagged_tweet_count = (@tweet_ids - @tweetids_with_hashtags).length
+
+        
+            render "static_pages/home"
+        end
     end
 end
