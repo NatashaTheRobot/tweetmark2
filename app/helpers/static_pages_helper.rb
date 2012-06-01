73dadce4c6793e7fc0b255d12b9eda8ffb03bb42
Natasha Murashev <nmurashev@gmail.com>
2012-06-01 12:28:16 -0700

    tweetmark is singular for 1

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,9 @@
 module StaticPagesHelper
+    def tweetmark_title(count)
+       if count == 1
+           return "tweetmark"
+        else
+            return "tweetmarks"
+        end 
+    end
 end
